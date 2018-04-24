f1d5175489bc379fc02165a8b74110a93c699590
mahdi3953 <Dr.mahdikarimi@gmail.com>
2018-04-25 00:51:15 +0430

    Update assignment4.py

--- assignment4.py
+++ assignment4.py
@@ -1,20 +1,21 @@
+import nmap
+
+
 def port_scanner():
-    """
-    write a port scanner using nmap library
-    for scanning 10 arbitrary hosts and and ports between
-    21-1000 then write the result to a file using
-    write_to_file function
-    """
+    All_Hosts = {'192.168.1.1','192.168.1.2','192.168.1.3','192.168.1.4'
+        ,'192.168.1.5','192.168.1.6','192.168.1.7',
+        '192.168.1.8','192.168.1.9','192.168.1.10'}
+    nm =  nmap.PortScanner()
+    for h in All_Hosts:
+        nm.scan(h,'21-1000')
+        content = "Host : %s = %s"%(h,nm.scaninfo()) + '\n'
+        write_to_file(content)
 
-def write_to_file(contents):
-    """
-    first remove the pass from function body
-    then write your code
-    :param contents: the result that should be written to file
-    :return: true if the writing successfull and false if can't
-    """
-    pass
 
+def write_to_file(content):
+    file = open('Result.txt','a')
+    file.write(content)
+    file.close()
 
 def main():
     port_scanner()
